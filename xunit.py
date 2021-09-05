34f5990491637b36daa339a360bdbf63f5f1bd54
ma38su <ma38su@gmail.com>
2021-09-05 21:37:07 +0900

    add testSetUp

--- xunit.py
+++ xunit.py
@@ -20,6 +20,12 @@ class TestCaseTest(TestCase):
         assert(not test.wasRun)
         test.run()
         assert(test.wasRun)
+    
+    def testSetUp(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert(test.wasSetup)
 
 if __name__ == '__main__':
     TestCaseTest("testRunning").run()
+    TestCaseTest("testSetup").run()
